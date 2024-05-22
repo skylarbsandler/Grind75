66eee9ad3780c29c441e4b399dbebeb958322a1f
skylarbsandler <skylarbsandler@gmail.com>
2024-05-22 15:34:38 -0600

    Added coding challenge prompt

--- TwoSum.cs
+++ TwoSum.cs
@@ -1,8 +1,14 @@
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
 public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
     {
-
         // iterates over each element in array, i as the index
         for (int i = 0; i < nums.Length; i++)
         {

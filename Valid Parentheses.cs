907ae5e1d7294730e47c4bb4f3a32d05f1f7391b
skylarbsandler <skylarbsandler@gmail.com>
2024-05-23 12:18:44 -0600

    Solved Valid Parentheses

--- Valid Parentheses.cs
+++ Valid Parentheses.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 //Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 //An input string is valid if:
@@ -5,4 +7,37 @@
 //Open brackets must be closed by the same type of brackets.
 //Open brackets must be closed in the correct order.
 //Every close bracket has a corresponding open bracket of the same type.
- 
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        //Map opening brackets to their corresponding closing brackets
+        var parenthesesDictionary = new Dictionary<char, char>()
+        {
+            {'[', ']'},
+            {'(', ')'},
+            {'{', '}'}
+        };
+
+        // Create stack to keep track of expected closing brackets
+        var endings = new Stack<char>();
+
+        // Iterate over each character in input
+        foreach (var current in s)
+        {
+            // If character is opening bracket
+            if (parenthesesDictionary.ContainsKey(current))
+            {
+                // Push closing bracket onto the stack
+                endings.Push(parenthesesDictionary[current]);
+            }
+            // If character is closing bracket
+            else if (endings.Count == 0 || endings.Pop() != current)
+            {
+                return false;
+            }
+        }
+        return endings.Count == 0;
+    }
+}
